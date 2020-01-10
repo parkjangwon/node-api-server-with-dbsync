64f036775c1bd046236bc63e6cd313f0174aeac4
parkjangwon <vim@kakao.com>
2020-01-10 22:18:07 +0900

    modify comment

--- bin/www.js
+++ bin/www.js
@@ -7,7 +7,7 @@
 const app = require('../server');
 const port = 3000;
 const syncDatabase = require('./sync-db');
-// 운영모드로 살행시 NODE=prod npm start 명령어를 사용한다.
+// 운영모드로 실행시 NODE=prod npm start 명령어를 사용한다.
 const nodeEnv = process.env.NODE_ENV || 'dev';
 
 app.listen(port, () => {
